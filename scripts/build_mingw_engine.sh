b5cce8fb009087bed2e36a19a0dd1e613489975b
mittorn <mittorn@sibmail.com>
2019-09-29 05:08:12 +0700

    ci: try fix mingw build

--- scripts/build_mingw_engine.sh
+++ scripts/build_mingw_engine.sh
@@ -11,10 +11,12 @@ export CXXFLAGS="-static-libgcc -static-libstdc++ -no-pthread -msse2"
 export LDFLAGS="-static-libgcc -static-libstdc++ -no-pthread -Wl,--allow-multiple-definition" # workaround some other mingw bugs
 export WINRC="i686-w64-mingw32-windres"
 rm -rf build # clean build directory
-./waf configure -s "SDL2_mingw/i686-w64-mingw32/" --build-type=none --prefix="." --win-style-install -v || die # can't compile VGUI support on MinGW, due to differnet C++ ABI
-./waf build -v || die
-cp $TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32//bin/SDL2.dll . # Install SDL2
-cp vgui_support_bin/vgui_support.dll .
-./waf install || die
+./waf configure -s "SDL2_mingw/i686-w64-mingw32/" --build-type=none --prefix="." --win-style-install || die # can't compile VGUI support on MinGW, due to differnet C++ ABI
+echo Configuration done
+./waf build || die
+echo Build done
+#cp $TRAVIS_BUILD_DIR/SDL2_mingw/i686-w64-mingw32//bin/SDL2.dll . # Install SDL2
+#cp vgui_support_bin/vgui_support.dll .
+#./waf install || die
 
-7z a -t7z $TRAVIS_BUILD_DIR/xash3d-mingw.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe
+#7z a -t7z $TRAVIS_BUILD_DIR/xash3d-mingw.7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on *.dll *.exe
